056c92a0af3eb73a0996b18e37426ac420929994
Dyhlio <dyhlio@proton.me>
2025-11-09 17:58:15 +0100

    Change error handling to use std::io::Error

--- src/config.rs
+++ src/config.rs
@@ -19,26 +19,36 @@ pub fn load_config() -> Result<Config, Box<dyn std::error::Error>> {
     }
 }
 
-pub fn load_config_from_env() -> Result<Config, String> {
+pub fn load_config_from_env() -> Result<Config, std::io::Error> {
     let username = std::env::var("YGG_USERNAME")
-        .map_err(|_| "YGG_USERNAME env var is undefined".to_string())?;
+        .map_err(|_| std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "YGG_USERNAME env var is undefined",
+        ))?;
 
     let password = std::env::var("YGG_PASSWORD")
-        .map_err(|_| "YGG_PASSWORD env var is undefined".to_string())?;
+        .map_err(|_| std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "YGG_PASSWORD env var is undefined",
+        ))?;
 
     let bind_ip = std::env::var("BIND_IP").unwrap_or("0.0.0.0".to_string());
 
     let bind_port = std::env::var("BIND_PORT")
         .unwrap_or("8715".to_string())
         .parse::<u16>()
-        .map_err(|_| "BIND_PORT must be a valid u16 number".to_string())?;
+        .map_err(|_| std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "BIND_PORT must be a valid u16 number",
+        ))?;
 
     let log_level = std::env::var("LOG_LEVEL")
         .unwrap_or("debug".to_string())
         .parse::<LevelFilter>()
-        .map_err(|_| {
-            "LOG_LEVEL must be a valid log level (off, error, warn, info, debug, trace)".to_string()
-        })?;
+        .map_err(|_| std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "LOG_LEVEL must be a valid log level (off, error, warn, info, debug, trace)",
+        ))?;
 
     Ok(Config {
         username,
