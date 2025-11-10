35d585fb272662eeb1434fef0dbc09a43e32cd14
UwUDev <mmimi0593@gmail.com>
2025-11-10 21:12:49 +0100

    UPX-optimized releases

--- compiling/compile-releases.sh
+++ compiling/compile-releases.sh
@@ -26,8 +26,17 @@ cp target/release/ygege releases/v$VERSION/ygege-linux-x86_64
 cp target/x86_64-pc-windows-gnu/release/ygege.exe releases/v$VERSION/ygege-windows-x86_64.exe
 cp target/armv7-unknown-linux-gnueabihf/release/ygege releases/v$VERSION/ygege-linux-armv7
 
+# Create UPX-optimized copies
+cp releases/v$VERSION/ygege-linux-x86_64 releases/v$VERSION/ygege-linux-x86_64-upx
+cp releases/v$VERSION/ygege-windows-x86_64.exe releases/v$VERSION/ygege-windows-x86_64-upx.exe
+cp releases/v$VERSION/ygege-linux-armv7 releases/v$VERSION/ygege-linux-armv7-upx
+
+# Optimize with UPX
+echo "Optimizing binaries with UPX..."
+upx --best --lzma releases/v$VERSION/ygege-linux-x86_64-upx
+upx --best --lzma releases/v$VERSION/ygege-windows-x86_64-upx.exe
+upx --best --lzma releases/v$VERSION/ygege-linux-armv7-upx
+
 echo "Releases built and stored in releases/v$VERSION/"
 
 cargo clean
-
-
