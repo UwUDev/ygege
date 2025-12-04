a004dda8328f82f4d4593eb3365e0fa27e88c9ac
thomas <wrocket>
2025-12-04 22:24:10 +0100

    translate flake description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Ygégé - Indexeur haute performance pour YGG Torrent";
+  description = "Ygégé - High-performance indexer for YGG Torrent";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
